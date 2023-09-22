ecb9502dbb07a80b366f7039ac13f2d3b174c42f
Don <don@dondeveloper.de>
2023-09-22 17:46:25 +0200

    Setup dotenv, express & ejs

---
+++ src/config.js
@@ -0,0 +1,4 @@
+import 'dotenv/config'
+
+export const PORT = process.env.PORT;
+export const HOSTNAME = process.env.HOSTNAME;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+import express from 'express';
+import * as config from './config.js'; 
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Listening on http://${config.HOSTNAME}:${config.PORT}`)
+})
